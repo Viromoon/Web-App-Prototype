706097662cb48acab6b4efe7b0548e51955fd5c2
Giulia <>
2021-05-23 12:59:33 +0000

    need to make the kanban board unscrollable and mach with phone media screen. need to add a pop up task list

---
+++ server.js
@@ -0,0 +1,110 @@
+//TASK FORM//
+const form = document.getElementById("taskform");
+const button = document.querySelector("#taskform > button")
+var taskInput = document.getElementById("taskInput");
+var taskList = document.getElementById("tasklist");
+var dueDateInput = document.getElementById("dueDateInput");
+var completionTimeInput = document.getElementById("completionTimeInput");
+var estimatedTimeInput = document.getElementById("estimatedTimeInput");
+var priorityInput = document.getElementById("priorityInput");
+
+button.addEventListener("click", function(event){
+  event.preventDefault();
+  let task = taskInput.value;
+  let dueDate = dueDateInput.value;
+  let completionTime = completionTimeInput.value;
+  let estimatedTime = estimatedTimeInput.value;
+  let priorityRating = priorityInput.options[priorityInput.selectedIndex].value;
+  addTask(task, dueDate, estimatedTime, priorityRating, completionTime, false);
+  console.log(taskList)
+})
+
+var taskListArray =[];
+
+function addTask(taskDescription, dueDate, estimatedTime, priorityRating, completionTime, completionStatus){
+  let d = new Date();
+  let dateCreated = d.getFullYear();
+  let task = {
+    taskDescription,
+    dueDate,
+    dateCreated,
+    estimatedTime,
+    completionTime,
+    priorityRating,
+    estimatedTime,
+    completionStatus
+  };
+  taskListArray.push(task);
+  renderTask(task);
+}
+
+function renderTask(task){
+  //Create HTML elements
+  let item = document.createElement("li");
+  item.innerHTML = "<p>" + task.taskDescription + "/p";
+
+  tasklist.appendChild(item);
+
+  //Extra Task DOM elements
+  let delButton = document.createElement("button");
+  let delButtonText = document.createTextNode("Delete Task");
+  delButton.appendChild(delButtonText);
+  item.appendChild(delButton);
+    
+  //Event Listemers for DOM elements
+  delButton.addEventListener("click", function(event){
+    event.preventDefault();
+    item.remove();
+})
+
+  // Clear the input form
+  form.reset();
+}
+
+
+//DRAG AND DROP//
+const list_items = document.querySelectorAll('.list-item');
+const lists = document.querySelectorAll('.list');
+
+let draggedItem = null;
+
+for (let i = 0; i < list_items.length; i++) {
+	const item = list_items[i];
+
+	item.addEventListener('dragstart', function () {
+		draggedItem = item;
+		setTimeout(function () {
+			item.style.display = 'none';
+		}, 0)
+	});
+
+	item.addEventListener('dragend', function () {
+		setTimeout(function () {
+			draggedItem.style.display = 'block';
+			draggedItem = null;
+		}, 0);
+	})
+
+	for (let j = 0; j < lists.length; j ++) {
+		const list = lists[j];
+
+		list.addEventListener('dragover', function (e) {
+			e.preventDefault();
+		});
+		
+		list.addEventListener('dragenter', function (e) {
+			e.preventDefault();
+			this.style.backgroundColor = 'rgba(0, 0, 0, 0.2)';
+		});
+
+		list.addEventListener('dragleave', function (e) {
+			this.style.backgroundColor = 'rgba(0, 0, 0, 0.1)';
+		});
+
+		list.addEventListener('drop', function (e) {
+			console.log('drop');
+			this.append(draggedItem);
+			this.style.backgroundColor = 'rgba(0, 0, 0, 0.1)';
+		});
+	}
+}
